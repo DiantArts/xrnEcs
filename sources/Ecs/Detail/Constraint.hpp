7af317b1ab441bf8bfcb07a1526b91b34a03a32f
DiantArts <DiantArts@gmail.com>
2022-03-05 00:53:15 +0100

    [Entity and Entity::Container]     Constraint         Adding: *_t and *_v helpers, and starting to use them as much as possible         Adding: Few useful constrains         Adding: Unit tests     Entity         Adding: Finishing the doxygen documentation     Entity::Container         Adding: Doxygen documentation         Adding: Few helper overloads

--- sources/Ecs/Detail/Constraint.hpp
+++ sources/Ecs/Detail/Constraint.hpp
@@ -1,10 +1,15 @@
 #pragma once
 
+///////////////////////////////////////////////////////////////////////////
+// Headers
+///////////////////////////////////////////////////////////////////////////
 #include <Meta/Constraint.hpp>
 #include <Meta/IsBaseOfTemplate.hpp>
 #include <Util/Time.hpp>
 
-
+///////////////////////////////////////////////////////////////////////////
+// Forward declerations
+///////////////////////////////////////////////////////////////////////////
 namespace xrn::ecs::entity { class Entity; }
 namespace xrn::ecs::component { template <typename T> class AComponent; }
 
@@ -12,25 +17,64 @@ namespace xrn::ecs::component { template <typename T> class AComponent; }
 
 namespace xrn::ecs::detail::constraint {
 
-
-
+///////////////////////////////////////////////////////////////////////////
+/// \brief Checks whether the Type given as template parameter is an
+///        ::xrn::ecs::entity::Entity
+///
+/// The comparison ignores cv-qualifiers and references (compares the type
+/// referenced).
+///
+/// \tparam Type to check
+///
+/// \return True if the Type given as template parameter is an
+///         ::xrn::ecs::entity::Entity. False otherwise
+///
+/// \see ::xrn::ecs::entity::Entity
+///
+///////////////////////////////////////////////////////////////////////////
 template <
     typename Type
 > concept isEntity =
     ::xrn::meta::constraint::sameAs<::xrn::ecs::entity::Entity, Type>;
 
-template <
-    typename Type
-> concept isComponent =
-    ::xrn::meta::IsBaseOfTemplate<::xrn::ecs::component::AComponent, Type>::value;
-
+///////////////////////////////////////////////////////////////////////////
+/// \brief Checks whether the Type given as template parameter is an
+///        ::xrn::util::Time
+///
+/// The comparison ignores cv-qualifiers and references (compares the type
+/// referenced).
+///
+/// \tparam Type to check
+///
+/// \return True if the Type given as template parameter is an
+///         ::xrn::util::Time. False otherwise
+///
+/// \see ::xrn::util::Time
+///
+///////////////////////////////////////////////////////////////////////////
 template <
     typename Type
 > concept isTime =
     ::xrn::meta::constraint::sameAs<::xrn::Time, Type>;
 
-
-
-
+///////////////////////////////////////////////////////////////////////////
+/// \brief Checks whether the Type given as template parameter inherits
+///        from ::xrn::ecs::component::AComponent
+///
+/// The comparison ignores cv-qualifiers and references (compares the type
+/// referenced).
+///
+/// \tparam Type to check
+///
+/// \return True if the Type given as template parameter inherits from
+///         ::xrn::ecs::component::AComponent. False otherwise
+///
+/// \see ::xrn::ecs::component::AComponent
+///
+///////////////////////////////////////////////////////////////////////////
+template <
+    typename Type
+> concept isComponent =
+    ::xrn::meta::IsBaseOfTemplate<::xrn::ecs::component::AComponent, Type>::value;
 
 } // namespace xrn::ecs::detail::constraint
---
+++ tests/Meta/Constraint.cpp
@@ -0,0 +1,56 @@
+#include <pch.hpp>
+#include <Meta/Constraint.hpp>
+
+#include <boost/test/unit_test.hpp>
+BOOST_AUTO_TEST_SUITE(test)
+BOOST_AUTO_TEST_SUITE(xrn)
+BOOST_AUTO_TEST_SUITE(meta)
+BOOST_AUTO_TEST_SUITE(constraint)
+
+
+
+BOOST_AUTO_TEST_CASE(sameAs)
+{
+    BOOST_TEST((::xrn::meta::constraint::sameAs<int, const int&>));
+    BOOST_TEST((::xrn::meta::constraint::sameAs<int, const int*>));
+    BOOST_TEST((::xrn::meta::constraint::sameAs<int, const int>));
+    BOOST_TEST((::xrn::meta::constraint::sameAs<int, int&>));
+    BOOST_TEST((::xrn::meta::constraint::sameAs<int, int*>));
+    BOOST_TEST((::xrn::meta::constraint::sameAs<int, int>));
+
+    BOOST_TEST((::xrn::meta::constraint::sameAs<const int&, int>));
+    BOOST_TEST((::xrn::meta::constraint::sameAs<const int*, int>));
+    BOOST_TEST((::xrn::meta::constraint::sameAs<const int, int>));
+    BOOST_TEST((::xrn::meta::constraint::sameAs<int&, int>));
+    BOOST_TEST((::xrn::meta::constraint::sameAs<int*, int>));
+    BOOST_TEST((::xrn::meta::constraint::sameAs<int, int>));
+}
+
+BOOST_AUTO_TEST_CASE(isConst)
+{
+    BOOST_TEST(!::xrn::meta::constraint::isConst<int>);
+    BOOST_TEST(!::xrn::meta::constraint::isConst<int&>);
+    BOOST_TEST(!::xrn::meta::constraint::isConst<int* const>);
+
+    BOOST_TEST(::xrn::meta::constraint::isConst<const int*>);
+    BOOST_TEST(::xrn::meta::constraint::isConst<const int&>);
+    BOOST_TEST(::xrn::meta::constraint::isConst<const int>);
+}
+
+BOOST_AUTO_TEST_CASE(isMutable)
+{
+    BOOST_TEST(!::xrn::meta::constraint::isMutable<int>);
+    BOOST_TEST(!::xrn::meta::constraint::isMutable<const int&>);
+    BOOST_TEST(!::xrn::meta::constraint::isMutable<const int*>);
+
+    BOOST_TEST(::xrn::meta::constraint::isMutable<int*>);
+    BOOST_TEST(::xrn::meta::constraint::isMutable<int&>);
+    BOOST_TEST(::xrn::meta::constraint::isMutable<int *const>);
+}
+
+
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
